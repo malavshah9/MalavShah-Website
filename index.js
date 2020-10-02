e10cb53b5c230c527241656f9100b19df2029b74
Malav Shah - Fintech <mshah@mittals.net>
2020-10-02 12:14:32 +0530

    :tada: fix analytics issue

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ import App from "./src/App";
 import ReactGA from "react-ga";
 import { config } from "dotenv";
 config();
-ReactGA.initialize(process.env.TRACKING_ID);
+ReactGA.initialize("UA-173521458-1");
 // console.log(process.env.TRACKING_ID);
 ReactDOM.render(<App />, document.getElementById("root"));
--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Name from "./Name";
 import Description from "./Description";
 import Links from "./Links";
 import "../style.css";
+import ReactGA from "react-ga";
 const App = () => {
+  useEffect(() => {
+    ReactGA.event({
+      category: "Site Loads",
+      action: "Site Loaded"
+    });
+    return () => {
+      ReactGA.event({
+        category: "Site Unloaded",
+        action: "Site Unloaded"
+      });
+    };
+  }, []);
   return (
     <React.Fragment>
       <Name />
